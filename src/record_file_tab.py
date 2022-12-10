3161cc2123e9a10b0ef71ddcc3e1112d8d9d4f23
WDW <boa3465@gmail.com>
2022-12-11 00:14:14 +0900

    show, hide, close에 따라서 핫키를 등록 해제하도록 작성

--- src/record_file_tab.py
+++ src/record_file_tab.py
@@ -1,9 +1,22 @@
 from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import QShowEvent, QHideEvent, QCloseEvent
 
 from form.record_file_form import Ui_record_file_form
-
+import hotkey
 
 class RecordFileTab(QWidget, Ui_record_file_form):
     def __init__(self):
         super(RecordFileTab, self).__init__()
         self.setupUi(self)
+
+    def showEvent(self, event: QShowEvent) -> None:
+        hotkey.register_hotkey(self.winId(), 0, hotkey.VK_OEM_5)
+        return super().showEvent(event)
+
+    def hideEvent(self, event : QHideEvent) -> None:
+        hotkey.unregister_hotkey(self.winId(), 0)
+        return super().hideEvent(event)
+
+    def closeEvent(self, event : QCloseEvent) -> None:
+        hotkey.unregister_hotkey(self.winId(), 0)
+        return super().closeEvent(event)
